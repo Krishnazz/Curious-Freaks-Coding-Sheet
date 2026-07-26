ef58b0e47c7aca7e7ad462f634a83643ddcbbaf1
Krishnazz <talktokrishna06@gmail.com>
2026-07-26 21:20:48 +0530

    Add Q1_OddorEven and Q2_LastDigit classes for basic number operations

---
+++ Basics/Q1_OddorEven.java
@@ -0,0 +1,8 @@
+package Basics;
+
+public class Q1_OddorEven{
+        static boolean isEven(int n) {
+        // code here
+        return n%2==0? true:false;
+    }
+}
---
+++ Basics/Q2_LastDigit.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.math.BigInteger;
+
+public class Q2_LastDigit{
+    static int getLastDigit(String a, String b) {
+        // code here
+          BigInteger base = new BigInteger(a);
+        BigInteger exponent = new BigInteger(b);
+        BigInteger modulus = BigInteger.TEN; 
+        
+        
+        BigInteger result = base.modPow(exponent, modulus);
+ 
+        int lastDigit = result.intValue();
+        
+        return lastDigit;
+    }
+}
