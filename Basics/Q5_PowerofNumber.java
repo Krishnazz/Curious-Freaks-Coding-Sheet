15a98db83a86120ad92764fd130999691b1b378c
Krishnazz <talktokrishna06@gmail.com>
2026-07-28 22:13:23 +0530

    Add Q5_PowerofNumber class for calculating power using reversed exponentiation

---
+++ Basics/Q5_PowerofNumber.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class Q5_PowerofNumber {
+    public int reverseExponentiation(int n) {
+        // code here
+        int rev=0,rem=0;
+        int num=n;
+        while(n>0){
+            rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+      
+       return  (int) Math.pow(num,rev);
+        
+    }
+}
