6197a77565501ec5f004f508fc4cf8759ad7ebbf
Krishnazz <talktokrishna06@gmail.com>
2026-08-05 08:40:36 +0530

    Implement isPerfectSquare method to check for perfect square numbers

--- Basics/Q12_PerfectSquare.java
+++ Basics/Q12_PerfectSquare.java
@@ -1,6 +1,24 @@
 class Q12_PerfectSquare {
     boolean isPerfectSquare(int n) {
-        // code here
-     
+
+        if (n == 1)
+            return false;
+
+        int sum = 1;
+
+        for (int i = 2; i*i<=n; i++) {
+            if (n % i == 0) {
+                sum += i;
+              
+               if(n/i!=i){
+               
+                sum+=n/i;
+              }
+            }
+            
+        }
+        //System.out.println(sum);
+
+        return sum == n;
     }
 }
