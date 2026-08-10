a2fd7288a28859c3a099bb9299f9fb52ed870dd6
Krishnazz <talktokrishna06@gmail.com>
2026-08-10 19:08:33 +0530

    Add Q1_MaxMin class to find maximum and minimum values in an array

--- Array - Data Structure/Traversal/Q1_MaxMin.java
+++ Array - Data Structure/Traversal/Q1_MaxMin.java
@@ -1,1 +1,13 @@
-    
+    import java.util.Arrays;
+    import java.util.HashMap;
+
+    class Q1_MaxMin {
+    public HashMap<String, Integer> getMinMax(int[] arr) {
+        // Code Here
+        Arrays.sort(arr);
+        HashMap<String, Integer> minMax = new HashMap<>();
+        minMax.put("min", arr[0]);
+        minMax.put("max", arr[arr.length - 1]);
+        return minMax;
+    }
+}
