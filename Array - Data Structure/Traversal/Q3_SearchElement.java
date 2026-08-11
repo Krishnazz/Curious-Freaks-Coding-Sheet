de0685e166ea838c97f90f6b7b528e3018f04487
Krishnazz <talktokrishna06@gmail.com>
2026-08-11 20:20:22 +0530

    Add Q3_SearchElement class to implement a search function for an array

---
+++ Array - Data Structure/Traversal/Q3_SearchElement.java
@@ -0,0 +1,10 @@
+class Q3_SearchElement   {
+    static int search(int arr[], int x) {
+        // code here
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x)
+                return i;
+        }
+        return -1;
+    }
+}
