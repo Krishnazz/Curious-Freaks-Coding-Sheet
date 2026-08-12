514bae84ac41b2397635b866c69dc529bc896c8d
Krishnazz <talktokrishna06@gmail.com>
2026-08-12 20:02:57 +0530

    Add Q4_MissingNum class to find the missing number in an array

---
+++ Array - Data Structure/Traversal/Q4_MissingNum.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+        Arrays.sort(arr);
+        for(int i=1;i<=arr.length;i++){
+            if(arr[i-1]!=i)
+                return i;
+        }
+        
+        return arr[arr.length-1]+1;
+    }
+}
