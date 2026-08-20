f70857bac647b40f845df3ad2162eb624b5257ef
Krishnazz <talktokrishna06@gmail.com>
2026-08-20 19:14:38 +0530

    Implement findTwoElement method to identify repeated and missing numbers in an array

--- Array - Data Structure/Traversal/Q5_RepeatNum.java
+++ Array - Data Structure/Traversal/Q5_RepeatNum.java
@@ -1,3 +1,26 @@
+
+import java.util.ArrayList;
+
 public class Q5_RepeatNum {
-    
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        // code here
+        int[] count=new int[arr.length+1];
+        int repeat=-1;
+        int missing=-1;
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        for(int i=1;i<=arr.length;i++){
+            if(count[i]==0){
+                missing=i;
+            }
+            if(count[i]==2){
+                repeat=i;
+            }
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(repeat);
+        result.add(missing);
+       return result;
+    }
 }
