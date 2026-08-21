73b863eca1fc85482e92906a964578a5df0d7c9c
Krishnazz <talktokrishna06@gmail.com>
2026-08-21 23:48:16 +0530

    Add Q7_EqualArrays class to implement equality check for two arrays

---
+++ Array - Data Structure/Traversal/Q7_EqualArrays.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Q7_EqualArrays {
+    public static boolean checkEqual(int[] a, int[] b) {
+        // code here
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
+    }
+}
