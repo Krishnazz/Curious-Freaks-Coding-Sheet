e1d3cfc8af29f1306a1ae30a0c917d7a4ad4fc44
Krishnazz <talktokrishna06@gmail.com>
2026-08-21 23:47:19 +0530

    Add Q6_SortArray012 class to implement sorting of an array containing 0s, 1s, and 2s

---
+++ Array - Data Structure/Traversal/Q6_SortArray012.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class  Q6_SortArray012{
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+    }
+}
