b77f7ba4915929e08d9361a31716688a2184c4eb
Krishnazz <talktokrishna06@gmail.com>
2026-08-26 21:41:30 +0530

    Remove placeholder comment from checkEqual method in Q7_EqualArrays class

--- Array - Data Structure/Traversal/Q7_EqualArrays.java
+++ Array - Data Structure/Traversal/Q7_EqualArrays.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 class Q7_EqualArrays {
     public static boolean checkEqual(int[] a, int[] b) {
-        // code here
         Arrays.sort(a);
         Arrays.sort(b);
         return Arrays.equals(a,b);
